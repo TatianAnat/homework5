154619f2b77b657ad329f9b162da14981955dab2
TatianAnat <TatianAnat@yandex.ru>
2025-04-06 19:09:15 +0400

    work number 5 task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //task1
+        int variable1 = 42500;
+        System.out.println("Значение переменной variable1 с типом int равно " + variable1);
+        byte variable2 = 56;
+        System.out.println("Значение переменной variable2 с типом byte равно " + variable2);
+        short variable3 = 28340;
+        System.out.println("Значение переменной variable3 с типом short равно " + variable3);
+        long variable4 = 247483649;
+        System.out.println("Значение переменной variable4 с типом long равно " + variable4);
+        float variable5 = 247.45f;
+        System.out.println("Значение переменной variable5 с типом float равно " + variable5);
+        double variable6 = 247.45f;
+        System.out.println("Значение переменной variable6 с типом double равно " + variable6);
     }
 }
