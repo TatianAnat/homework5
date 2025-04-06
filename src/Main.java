ff4e6a76e4d890486ce10eefb81e8a1013f82fd5
TatianAnat <TatianAnat@yandex.ru>
2025-04-06 19:21:54 +0400

    work number 5 task 2

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,27 @@ public class Main {
         System.out.println("Значение переменной variable2 с типом byte равно " + variable2);
         short variable3 = 28340;
         System.out.println("Значение переменной variable3 с типом short равно " + variable3);
-        long variable4 = 247483649;
+        long variable4 = 247483649L;
         System.out.println("Значение переменной variable4 с типом long равно " + variable4);
         float variable5 = 247.45f;
         System.out.println("Значение переменной variable5 с типом float равно " + variable5);
         double variable6 = 247.45f;
         System.out.println("Значение переменной variable6 с типом double равно " + variable6);
+
+        //task2
+        float numberValue1 = 27.12f;
+        System.out.println(numberValue1);
+        long numberValue2 = 987678965549L;
+        System.out.println(numberValue2);
+        double numberValue3 = 2.786;
+        System.out.println(numberValue3);
+        short numberValue4 = 569;
+        System.out.println(numberValue4);
+        short numberValue5 = -159;
+        System.out.println(numberValue5);
+        short numberValue6 = 27897;
+        System.out.println(numberValue6);
+        byte numberValue7 = 67;
+        System.out.println(numberValue7);
     }
 }
